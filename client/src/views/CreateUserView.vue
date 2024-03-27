7ca38601ed9a44036f2d7fee0e624bee38288e16
FedeMontenegro <fedejulianmontenegro@gmail.com>
2024-03-27 01:48:42 -0300

    Create user view completed.

--- client/src/views/CreateUserView.vue
+++ client/src/views/CreateUserView.vue
@@ -1,3 +1,19 @@
+<script setup>
+    import CreateUserView from "../components/CreateUserForm.vue";
+
+    const elements = ["Employee", "Superadmin"]
+</script>
+
 <template>
-    <p>create user</p>
+    <div>
+        <h2 class="create-user-title">Create User</h2>
+        <CreateUserView />
+    </div>
 </template>
+
+<style scoped>
+    .create-user-title {
+        padding: 20px;
+        text-align: center;
+    }
+</style>
